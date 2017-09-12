af6203c71e0f0fa2e680adec7d48b3771f843d34
Charlie DeRiggs <charlie.deriggs@gmail.com>
2017-09-12 18:55:04 -0400

    improved performance time to 30

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var port = process.env.PORT || 8081;
 var rule = new schedule.RecurrenceRule();
 
 
-rule.minute = new schedule.Range(0, 60, 5);
+rule.minute = new schedule.Range(0, 60, 30);
 
 
 var dailyJob = schedule.scheduleJob(rule, function(){
